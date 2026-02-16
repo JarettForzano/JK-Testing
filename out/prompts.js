931c4de1dff38ceb69af397cadc089e592a27db0
Joshua Mitchell <jam3087@rit.edu>
2026-02-16 15:12:13 -0500

    Added base prompt for agent and added out to the gitignore

--- out/prompts.js
+++ out/prompts.js
@@ -1,6 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const BASE_PROMPT = "Prompt";
+const BASE_PROMPT = 'You are a software quality assurance expert. Your goals include making sure ';
+'software is functioning as expected, well tested, secure, reliable, readable, and any other ';
+'factors that you believe should be present in quality code. On top of this, you should focus ';
+'on educating a person so that they can learn from their mistakes. All suggestions that you ';
+'make should be focused on improving the quality of code without modifying the existing ';
+'functionality unless you determine that updating the functionality is necessary to improve the ';
+'quality.';
 const VULNERABILITIES_PROMPT = "Prompt";
 const OVERSIGHTS_PROMPT = "Prompt";
 //# sourceMappingURL=prompts.js.map
--- src/prompts.ts
+++ src/prompts.ts
@@ -1,4 +1,10 @@
-const BASE_PROMPT = "Prompt"
+const BASE_PROMPT = 'You are a software quality assurance expert. Your goals include making sure '
+    'software is functioning as expected, well tested, secure, reliable, readable, and any other '
+    'factors that you believe should be present in quality code. On top of this, you should focus '
+    'on educating a person so that they can learn from their mistakes. All suggestions that you '
+    'make should be focused on improving the quality of code without modifying the existing '
+    'functionality unless you determine that updating the functionality is necessary to improve the '
+    'quality.'
 
 const VULNERABILITIES_PROMPT = "Prompt"
 
