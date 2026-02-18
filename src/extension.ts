991085eb78fc7bd2800cc1cb27d82e9e57ca6c77
kindlywouldyou <khandakerfahmid03@gmail.com>
2026-02-18 14:45:01 -0500

    Revert "Added command to right click context menu, needs to be connected to a…"

--- src/extension.ts
+++ src/extension.ts
@@ -8,34 +8,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-tutor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-tutor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-tutor!');
-	});
-
-	context.subscriptions.push(disposable);
-
-	// Register the "Test with JK-Testing" context menu command
-	const jkTestingCommand = vscode.commands.registerCommand('code-tutor.testWithJK', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const selection = editor.selection;
-			const selectedText = editor.document.getText(selection);
-			// TODO: wire selectedText into your LLM/testing logic
-			vscode.window.showInformationMessage('JK-Testing triggered!');
-		}
-	});
-
-	context.subscriptions.push(jkTestingCommand);
 	console.log('Congratulations, your extension "jk-test" is now active!');
 }
-
 // define a chat handler
 const base_handler: vscode.ChatRequestHandler = async (
   request: vscode.ChatRequest,
