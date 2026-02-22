69ecef3c8091cff90c2458709feff5dfda0ca3ea
Joshua Mitchell <jam3087@rit.edu>
2026-02-22 16:34:58 -0500

    Added menu workflow so user can choose if they want to run the check on changes in a new commit. They can choose Yes, Always, No, and Never. Implementation should be complete

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
 import { BASE_PROMPT, VULNERABILITIES_PROMPT, OVERSIGHTS_PROMPT, ALL } from './prompts';
-import { GitExtension } from './git';
 import { trackCommits } from './versionControl';
 
 // This method is called when your extension is activated
@@ -12,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "jk-test" is now active!');
 
-  trackCommits();
+  trackCommits(context);
 }
 // define a chat handler
 const base_handler: vscode.ChatRequestHandler = async (
--- src/versionControl.ts
+++ src/versionControl.ts
@@ -3,10 +3,18 @@ import * as vscode from 'vscode';
 import { GitExtension, Repository } from './git';
 import * as path from 'path';
 
-export function trackCommits() {
+/**
+ * Set up a listener that will track when a new commit is added and ask the user if they
+ * want the JKAgent to check their work. 
+ * @returns void
+ */
+export function trackCommits(context: vscode.ExtensionContext) {
     const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
     const git = gitExtension?.getAPI(1);
+    context.globalState.update("ALWAYS", false);
+    context.globalState.update("NEVER", false);
 
+    // Failure to get the git api
     if (!git) return;
 
     // setup listening for a new repository that is opened by VSCode
@@ -24,13 +32,14 @@ export function trackCommits() {
 
             const currentHead = repo.state.HEAD;
 
-            // New commit was added
-            if (currentHead?.commit !== prevHead?.commit && currentHead?.name === prevHead?.name) {
-                vscode.window.showInformationMessage("A commit was added!");
+            // Helper function to run a full QA check with updates files as context
+            const checkAll = async () => {
                 if (prevHead?.commit && currentHead?.commit) {
                     let changes = '';
                     let diff = await repo.diffBetween(prevHead.commit, currentHead.commit);
                     let folderPath = vscode.workspace.workspaceFolders?.[0]?.uri.path;
+
+                    // Gather file names and content to add as context for the agent
                     for (let change of diff) {
                         let filepath = change.uri.path;
                         if (!folderPath) {
@@ -41,8 +50,31 @@ export function trackCommits() {
                         changes += "File: " + filepath + "\nContents: " + contents;
                     }
 
-                    vscode.commands.executeCommand("workbench.action.chat.open", "@JKAgent /all " + "Perform your tasks on the following fies that were updated:\n" + changes);
-                    
+                    vscode.commands.executeCommand("workbench.action.chat.open", "@JKAgent /all " + "Perform your tasks on the following fies that were updated:\n" + changes);  
+                }
+            }
+
+            // New commit was added
+            if (currentHead?.commit !== prevHead?.commit && currentHead?.name === prevHead?.name) {
+                // Run full check if "Always" was already selected
+                if (context.globalState.get("ALWAYS") == true) {
+                    checkAll();
+                    // Show options as long as "Never" wasn't selected
+                } else if (context.globalState.get("NEVER") == false) {
+                    let options = vscode.window.showInformationMessage("A new commit was detected. Run a QA check on all changes now?", "Yes", "Always", "No", "Never");
+                    await options.then(async (value) => {
+                        // Run /all command with the agent if the user chooses
+                        if (value === "Yes") {
+                            checkAll();
+                        } else if (value === "Always") {
+                                context.globalState.update("ALWAYS", true);
+                                context.globalState.update("NEVER", false);
+                                checkAll();
+                        } else if (value == "Never") {
+                            context.globalState.update("NEVER", true);
+                            context.globalState.update("ALWAYS", false);
+                        }
+                    })
                 }
                 
             }
