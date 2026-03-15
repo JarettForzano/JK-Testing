e2be67d59965ff51a92cd932d2c12ec9c07200ad
jacob-fay <71296174+jacob-fay@users.noreply.github.com>
2026-03-15 14:38:31 -0400

    hopefully fixed

--- autoVersioning.py
+++ autoVersioning.py
@@ -7,11 +7,7 @@ with open("package.json","r") as file:
     string = file.read()
     package = json.loads(string)
 version = [int(x) for x in package["version"].split(".")]
-if (versionType == "major"):
-    package["version"] = f"{version[0]+1}.0.0"
-elif (version[2] == "9"):
-    package["version"] = f"{version[0]}.{version[1]+1}.0"
-else:
-    package["version"] = f"{version[0]}.{version[1]}.{version[2]+1}"
+package["version"] = versionType
+
 with open("package.json","w") as file:
     file.write(json.dumps(package))
